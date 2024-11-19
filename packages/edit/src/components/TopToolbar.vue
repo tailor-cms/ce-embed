b63771b89c7a81efb4d91ee57f407b3701ca2773
rkusan00 <renato.kusanovic@gmail.com>
2024-11-19 16:08:35 +0100

    Use form for toolbar

--- packages/edit/src/components/TopToolbar.vue
+++ packages/edit/src/components/TopToolbar.vue
@@ -1,34 +1,76 @@
 <template>
-  <div class="d-flex align-center justify-center flex-fill ga-4">
-    <VTextField
-      :model-value="element.data.height"
-      class="required"
-      hide-details="auto"
-      label="Height"
-      max-width="224"
-      prepend-inner-icon="mdi-arrow-expand-vertical"
-      suffix="px"
-      type="number"
-      variant="outlined"
-      @update:model-value="$emit('save', { ...element.data, height: $event })"
-    />
-    <VTextField
-      :model-value="element.data.url"
-      hide-details="auto"
-      label="URL"
-      max-width="600"
-      prepend-inner-icon="mdi-link-variant"
-      variant="outlined"
-      @update:model-value="$emit('save', { ...element.data, url: $event })"
-    />
-  </div>
+  <VForm ref="form" validate-on="submit" @submit.prevent="save">
+    <VToolbarItems class="ga-2">
+      <VTextField
+        v-model="elementData.height"
+        :disabled="!isEditing"
+        :rules="[rules.required]"
+        class="required"
+        hide-details="auto"
+        label="Height"
+        min-width="200"
+        prepend-inner-icon="mdi-arrow-expand-vertical"
+        suffix="px"
+        type="number"
+        variant="outlined"
+      />
+      <VTextField
+        v-model="elementData.url"
+        :disabled="!isEditing"
+        :rules="[rules.required, rules.url]"
+        hide-details="auto"
+        label="URL"
+        min-width="400"
+        prepend-inner-icon="mdi-link-variant"
+        variant="outlined"
+      />
+      <VBtn v-if="!isEditing" @click="isEditing = true">Edit</VBtn>
+      <template v-else-if="isDirty">
+        <VBtn @click="save">Save</VBtn>
+        <VBtn @click="cancel">Cancel</VBtn>
+      </template>
+    </VToolbarItems>
+  </VForm>
 </template>
 
 <script setup lang="ts">
-import type { Element } from '@tailor-cms/ce-embed-manifest';
+import { computed, reactive, ref } from 'vue';
+import type { Element, ElementData } from '@tailor-cms/ce-embed-manifest';
+import cloneDeep from 'lodash/cloneDeep';
+import isEqual from 'lodash/isEqual';
+import isURL from 'validator/lib/isURL';
 
-defineProps<{ element: Element }>();
-defineEmits(['save']);
+const rules = {
+  required: (val: string) => !!val || 'The field is required.',
+  url: (val: string) =>
+    isURL(val, {
+      protocols: ['http', 'https'],
+      require_protocol: true,
+      require_valid_protocol: true,
+    }) || 'URL is not valid.',
+};
+
+const props = defineProps<{ element: Element }>();
+const emit = defineEmits(['save']);
+
+const isEditing = ref(false);
+const form = ref<HTMLFormElement>();
+const elementData = reactive<ElementData>(cloneDeep(props.element.data));
+
+const isDirty = computed(() => !isEqual(elementData, props.element.data));
+
+const save = async () => {
+  const { valid } = await form.value?.validate();
+  if (!valid) return;
+  emit('save', elementData);
+  isEditing.value = false;
+};
+
+const cancel = () => {
+  Object.assign(elementData, cloneDeep(props.element.data));
+  form.value?.resetValidation();
+  isEditing.value = false;
+};
 </script>
 
 <style scoped></style>
